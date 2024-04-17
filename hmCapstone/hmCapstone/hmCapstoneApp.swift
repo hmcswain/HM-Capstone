857822fb8133b6a3384a8e4017c648ca2126ef65
hmcswain <hmcswain@gmail.com>
2024-04-17 11:26:37 -0700

    initial commit of Capstone file-blank
    initial commit of Capstone file-blank with testing enabled

---
+++ hmCapstone/hmCapstone/hmCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  hmCapstoneApp.swift
+//  hmCapstone
+//
+//  Created by Jj on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct hmCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
